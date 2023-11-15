a6e67eff8ce23d409db5a5d8b74cd88521f1f14a
Andrikin <contasecretaalternativa@gmail.com>
2023-11-15 14:19:07 -0300

    refac: removendo imports antigos

--- nvim/config/nvim/lua/andrikin/init.lua
+++ nvim/config/nvim/lua/andrikin/init.lua
@@ -1,8 +1,6 @@
 -- Chamar todos os arquivos
 require('andrikin.os')
 require('andrikin.lazy')
-require('andrikin.color')
-require('andrikin.lualine')
 require('andrikin.options')
 require('andrikin.maps')
 require('andrikin.autocmds')
