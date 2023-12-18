7c9bab7c22bd6b95eddf7fe3790854d330e4b504
Andrikin <contasecretaalternativa@gmail.com>
2023-12-18 14:18:52 -0300

    feat: instalacao de fonte no sistema
    Verificação e instalação da fonte SauceCodePro no sistema. Será
    instalado no usuário local, com registro no regedit, e mantendo os
    arquivos em %LOCALAPPDATA%/Microsoft/Windows/Fonts.

--- nvim/config/nvim/lua/andrikin/os.lua
+++ nvim/config/nvim/lua/andrikin/os.lua
@@ -1,3 +1,8 @@
+-- TODO: Verificar se existe as dependências e, caso contrário, realizar o download com o curl.
+local notify = function(msg)
+	vim.notify(msg)
+	vim.cmd.redraw({bang = true})
+end
 local Path = vim.F.npcall(require, 'plenary.path')
 if not Path then
 	error('Init: Não foi encontrado o plugin Plenary!')
@@ -91,39 +96,116 @@ for _, dep in ipairs(NVIM_DEPS) do
 	dep.config(dep.args)
 end
 
--- TODO: Verificar se está no sistema Windows
--- wip: passar código para plugin da Ouvidoria
--- Inicialização do NexusFont para uso da fonte SauceNerdPro
-local check_font_reg = {
-	'reg',
-	'query',
-	'"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts"',
-	'/s',
-	'|',
-	'find',
-	'"SauceCodePro"'
-}
-local has_font_reg = vim.fn.system(check_font_reg) ~= ''
-local has_font_local = vim.fn.glob(
-	'C:/Users/' .. vim.env.USERNAME .. '/AppData/Local/Microsoft/Windows/Fonts/SauceCodePro*',
-	false,
-	true
-)
-local has_font = has_font_reg or (#has_font_local > 0)
-if not has_font then
-	local query = {
-		'tasklist',
-		'/fo',
-		'list',
-		'/fi',
-		'"STATUS eq running"',
-		'|',
-		'find',
-		'"nexusfont.exe"'
-	}
-	vim.fn.system(query)
-	local has_nexusfont = vim.v.shell_error == 0
-	if not has_nexusfont then
-		vim.fn.jobstart('nexusfont.exe', { detach = true })
+-- Instalação da fonte SauceCodePro no computador
+local SauceCodePro = {}
+
+SauceCodePro.DIRECTORY = vim.env.LOCALAPPDATA .. '/Microsoft/Windows/Fonts'
+SauceCodePro.ZIP = SauceCodePro.DIRECTORY .. '/SauceCodePro.zip'
+SauceCodePro.REGISTRY = 'HKCU\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts'
+
+SauceCodePro.fonte_extraida = function(self)
+	return #(vim.fn.glob(self.DIRECTORY .. '/SauceCodePro*.ttf', false, true)) > 0
+end
+
+SauceCodePro.zip_encontrado = function(self)
+	return vim.loop.fs_stat(self.ZIP)
+end
+
+SauceCodePro.download = function(self)
+	-- Realiza download em AppData/Local/Microsoft/Windows/Fonts
+	if self:fonte_extraida() then
+		notify('Encontrado fonte SauceCodePro neste computador!')
+		return
+	end
+	if vim.fn.isdirectory(self.DIRECTORY) == 0 then
+		vim.fn.mkdir(self.DIRECTORY, 'p', 0700)
+	end
+	-- Realizar download da fonte
+	vim.fn.system({
+		'curl',
+		'--fail',
+		'--location',
+		'--silent',
+		'--output',
+		self.ZIP,
+		'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/SourceCodePro.zip'
+	})
+	if not self:zip_encontrado() then
+		error('Não foi possível realizar o download do arquivo da fonte.')
+	end
+	notify('Arquivo fonte SauceCodePro baixado!')
+end
+
+SauceCodePro.extrair = function(self)
+	-- Decompressar arquivo zip
+	-- Appdata/Local
+	if not self:zip_encontrado() then
+		notify('Arquivo SauceCodePro.zip não encontrado! Realizar o download do arquivo para continuar a intalação.')
+		return
+	end
+	vim.fn.system({
+		'unzip',
+		self.ZIP,
+		'-d',
+		self.DIRECTORY
+	})
+	if self:fonte_extraida() then
+		notify('Arquivo fonte SauceCodePro.zip extraído!')
+		self.FILES = vim.fn.glob(self.DIRECTORY .. '/SauceCodePro*.ttf', false, true)
+	else
+		error('Não foi possível extrair os arquivo de fonte SauceCodePro.')
 	end
 end
+
+SauceCodePro.fonte_intalada_regedit = function(self)
+	-- Verificando se a fonte SauceCodePro está intalada no computador
+	local lista = vim.tbl_filter(
+		function(elemento)
+			return elemento:match('SauceCodePro')
+		end,
+		vim.fn.systemlist({
+			'reg',
+			'query',
+			self.REGISTRY,
+			'/s'
+	}))
+	return #lista > 0
+end
+
+SauceCodePro.registrar = function(self)
+	-- Registra as fontes no RegEdit do sistema.
+	if self:fonte_intalada_regedit() then
+		notify('Fonte já registrada no sistema regedit deste computador!')
+		return
+	end
+	for _, fonte in ipairs(self.FILES) do
+		local arquivo = vim.fn.fnamemodify(fonte, ':t')
+		local diretorio = string.gsub(self.DIRECTORY .. '/' .. arquivo, '/', '\\')
+		vim.fn.system({
+			'reg',
+			'add',
+			self.REGISTRY,
+			'/v',
+			arquivo:match('(.*)%..*$') .. ' (TrueType)', -- nome de registro da fonte
+			'/t',
+			'REG_SZ',
+			'/d',
+			diretorio,
+			'/f'
+		})
+	end
+end
+
+SauceCodePro.instalar = function(self)
+	self:download()
+	self:extrair()
+	self:registrar()
+end
+
+local has_curl = vim.fn.executable('curl') == 1
+if has_curl then
+	SauceCodePro:instalar()
+else
+	notify('Não foi possível instalar a fonte SauceCodePro neste computador.')
+end
+
