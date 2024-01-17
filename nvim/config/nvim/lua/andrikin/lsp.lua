f9e16593e30a0813f02f867ff771a249d6e8ff6a
Andrikin <contasecretaalternativa@gmail.com>
2024-01-17 02:01:53 -0300

    refac(nvim-cmp): removido has_words_before

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -1,11 +1,5 @@
 -- Configuração de LSP servers
 
-local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local notify = function(msg)
 	vim.notify(msg)
 	vim.cmd.redraw({bang = true})
@@ -45,8 +39,6 @@ cmp.setup({
                 -- that way you will only jump inside the snippet region
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
