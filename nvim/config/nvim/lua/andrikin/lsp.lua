6436da914e4e87f40f48b1393bd93b59836854e2
Andrikin <contasecretaalternativa@gmail.com>
2024-01-19 15:17:32 -0300

    feat(nvim-cmp): CONSEGUI PORRA!
    Ajuste de configuração para trazer o auto preenchimento do plugin
    nvim-cmp, juntamente com os templates do plugin luasnip.

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -116,6 +116,12 @@ local luasnip = require("luasnip")
 require('luasnip.loaders.from_vscode').lazy_load() -- carregar snippets (templates)
 luasnip.config.setup({})
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("[a-zA-Z0-9._]") ~= nil
+end
+
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
@@ -127,15 +133,38 @@ cmp.setup({
         end,
     },
     completion = {
-        completeopt = 'menu,menuone,noinsert',
+        completeopt = vim.o.completeopt,
     },
     window = {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<c-n>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+                -- that way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ['<c-p>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        -- ['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), -- backup
+        -- ['<C-p>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), -- backup
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
