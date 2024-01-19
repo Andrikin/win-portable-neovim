2f2aace3adbad65d7b83fbbdccfa12762d2ef177
Andrikin <contasecretaalternativa@gmail.com>
2024-01-19 13:20:55 -0300

    refac(lsp): organizacao do codigo

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -5,162 +5,13 @@ local notify = function(msg)
 	vim.cmd.redraw({bang = true})
 end
 
-local cmp = require('cmp')
-local luasnip = require("luasnip")
-require('luasnip.loaders.from_vscode').lazy_load() -- carregar snippets (templates)
-luasnip.config.setup({})
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    completion = {
-        autocomplete = true,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<c-n>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<c-p>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip', option = { show_autosnippets = true } }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-        }, {
-            { name = 'path' },
-    })
-})
--- Set up lspconfig.
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- LSP SERVERS 
-local lsp = require('lspconfig')
-
--- Emmet LSP
-lsp.emmet_ls.setup({ -- npm install -g emmet-ls
-    capabilities = capabilities
-})
-
--- VSCODE LSP PACKAGE
-lsp.html.setup({ -- npm i -g vscode-langservers-extracted
-    capabilities = capabilities
-})
-lsp.jsonls.setup({ -- npm i -g vscode-langservers-extracted
-    capabilities = capabilities
-})
-lsp.cssls.setup({ -- npm i -g vscode-langservers-extracted
-    capabilities = capabilities
-})
--- Utilizar denols
--- lsp.eslint.setup({ -- npm i -g vscode-langservers-extracted
---     capabilities = capabilities,
---     on_attach = function(_, bufnr)
---         vim.api.nvim_create_autocmd("BufWritePre", {
---             buffer = bufnr,
---             command = "EslintFixAll",
---         })
---     end,
--- })
-
--- Python LSP
-lsp.pyright.setup({ -- pip install pyright | npm -g install pyright
-    capabilities = capabilities
-})
-
--- Javascript LSP
-lsp.denols.setup({
-    capabilities = capabilities
-})
-
--- Lua LSP
-lsp.lua_ls.setup({
-    capabilities = capabilities,
-    on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                Lua = {
-                    -- completion = {
-                    --     callSnippet = 'Replace',
-                    -- },
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = 'LuaJIT'
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        }
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    }
-                }
-            })
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        end
-        return true
-    end
-})
-
--- Java LSP
-lsp.jdtls.setup({
-    capabilities = capabilities
-})
-
--- Vim LSP
-lsp.vimls.setup({
-    capabilities = capabilities
-})
-
--- LaTeX LSP
-lsp.texlab.setup({
-    capabilities = capabilities
-})
-
--- Rust LSP
--- lsp.rust_analyzer.setup({})
-
 -- colorizer.lua
 require('colorizer').setup(nil, { css = true })
 
 -- Neodev
 require('neodev').setup()
 
-vim.defer_fn(
+vim.defer_fn( -- kickstart.nvim
     function()
         -- require('nvim-treesitter.install').compilers = {'clang', 'gcc'}
         require('nvim-treesitter.configs').setup({
@@ -258,4 +109,149 @@ if not ok then
 else
     notify('Telescope: extenção fzf carregada com sucesso')
 end
+
+local cmp = require('cmp')
+local luasnip = require("luasnip")
+
+require('luasnip.loaders.from_vscode').lazy_load() -- carregar snippets (templates)
+luasnip.config.setup({})
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip', option = { show_autosnippets = true } }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+        }, {
+            { name = 'path' },
+    })
+})
+
+-- Set up lspconfig.
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- LSP SERVERS 
+local lsp = require('lspconfig')
+
+-- Emmet LSP
+lsp.emmet_ls.setup({ -- npm install -g emmet-ls
+    capabilities = capabilities
+})
+
+-- Python LSP
+lsp.pyright.setup({ -- pip install pyright | npm -g install pyright
+    capabilities = capabilities
+})
+
+-- Javascript LSP
+lsp.denols.setup({
+    capabilities = capabilities
+})
+
+-- LaTeX LSP
+lsp.texlab.setup({
+    capabilities = capabilities
+})
+
+-- Java LSP
+lsp.jdtls.setup({
+    capabilities = capabilities
+})
+
+-- Vim LSP
+lsp.vimls.setup({
+    capabilities = capabilities
+})
+
+-- VSCODE LSP PACKAGE
+-- HTML
+lsp.html.setup({ -- npm i -g vscode-langservers-extracted
+    capabilities = capabilities
+})
+-- JSON
+lsp.jsonls.setup({ -- npm i -g vscode-langservers-extracted
+    capabilities = capabilities
+})
+-- CSS
+lsp.cssls.setup({ -- npm i -g vscode-langservers-extracted
+    capabilities = capabilities
+})
+-- WARNING: Utilizar denols
+-- JS
+-- lsp.eslint.setup({ -- npm i -g vscode-langservers-extracted
+--     capabilities = capabilities,
+--     on_attach = function(_, bufnr)
+--         vim.api.nvim_create_autocmd("BufWritePre", {
+--             buffer = bufnr,
+--             command = "EslintFixAll",
+--         })
+--     end,
+-- })
+
+-- -- Rust LSP
+-- lsp.rust_analyzer.setup({
+--    capabilities = capabilities
+-- })
+
+-- Lua LSP
+lsp.lua_ls.setup({
+    capabilities = capabilities,
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                    -- completion = {
+                    --     callSnippet = 'Replace',
+                    -- },
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    telemetry = { enable = false },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                }
+            })
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end
+        return true
+    end
+})
 
