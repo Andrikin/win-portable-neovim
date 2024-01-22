f9da1b746ccb9b4bcae5a470ee91c192ecb0e8bb
Andrikin <contasecretaalternativa@gmail.com>
2024-01-22 17:50:25 -0300

    refac(lsp): configuracao folke/neodev
    Para sugestões da biblioteca lua.

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -253,22 +253,25 @@ lsp.lua_ls.setup({
         if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
             client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
                 Lua = {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = 'LuaJIT'
+                    completion = { -- folke/neodev configuration
+                        callSnippet = "Replace"
                     },
+                    -- runtime = { -- comentado para funcionamento do neodev
+                    --     -- Tell the language server which version of Lua you're using
+                    --     -- (most likely LuaJIT in the case of Neovim)
+                    --     version = 'LuaJIT'
+                    -- },
                     -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                            -- '${3rd}/luv/library'
-                            -- '${3rd}/busted/library',
-                        }
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file('', true)
-                    },
+                    -- workspace = { -- comentado para funcionamento do neodev
+                    --     checkThirdParty = false,
+                    --     library = {
+                    --         vim.env.VIMRUNTIME
+                    --         -- '${3rd}/luv/library'
+                    --         -- '${3rd}/busted/library',
+                    --     }
+                    --     -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                    --     -- library = vim.api.nvim_get_runtime_file('', true)
+                    -- },
                     diagnostics = {
                         -- Get the language server to recognize the `vim` global
                         globals = {'vim'},
