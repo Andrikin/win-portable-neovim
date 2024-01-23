9cc84951b735ee0db618a3cf1e793fd826e9a0e7
Andrikin <contasecretaalternativa@gmail.com>
2024-01-23 18:58:19 -0300

    todo(utils): estudar nvim_echo
    Para melhorar função vim.notify

--- nvim/config/nvim/lua/andrikin/utils.lua
+++ nvim/config/nvim/lua/andrikin/utils.lua
@@ -4,7 +4,7 @@ local Utils = {}
 ---@param msg string
 Utils.notify = function(msg)
 	vim.cmd.mode()
-	vim.notify(msg)
+	vim.notify(msg) -- TODO: Verificar nvim_echo
 end
 
 Utils.npcall = vim.F.npcall
