1ab80aecde4a519f71cc1c8d32b8c0807c762ad0
Andrikin <contasecretaalternativa@gmail.com>
2024-01-24 13:21:57 -0300

    refac(utils): massive redraw

--- nvim/config/nvim/lua/andrikin/utils.lua
+++ nvim/config/nvim/lua/andrikin/utils.lua
@@ -4,15 +4,17 @@ local Utils = {}
 --- Mostra notificação para usuário, registrando em :messages
 ---@param msg string
 Utils.notify = function(msg)
-	vim.cmd.mode()
+    vim.cmd.redraw({bang = true})
     vim.api.nvim_echo({{msg, 'DiagnosticInfo'}}, true, {})
+    vim.cmd.redraw({bang = true})
 end
 
 --- Mostra uma notificação para o usuário, mas sem registrar em :messages
 ---@param msg string
 Utils.echo = function(msg)
-	vim.cmd.mode()
+    vim.cmd.redraw({bang = true})
     vim.api.nvim_echo({{msg, 'DiagnosticInfo'}}, false, {})
+    vim.cmd.redraw({bang = true})
 end
 
 Utils.npcall = vim.F.npcall
