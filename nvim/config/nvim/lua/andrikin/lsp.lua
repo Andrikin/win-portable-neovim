b57183664aae4bf2224853bc848c97a9a9710b5b
Andrikin <contasecretaalternativa@gmail.com>
2024-01-24 00:29:05 -0300

    wip(nvim-buffer): tentando fazer funcionar... :/

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -168,19 +168,20 @@ cmp.setup({
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
         }, {
+            -- { name = 'buffer' },
             { name = 'path' },
-            {
-                name = 'buffer',
-                -- option = {
-                --     get_bufnrs = function()
-                --         local bufs = {}
-                --         for _, win in ipairs(vim.api.nvim_list_wins()) do
-                --             bufs[vim.api.nvim_win_get_buf(win)] = true
-                --         end
-                --         return vim.tbl_keys(bufs)
-                --     end
-                -- }
-            },
+            -- {
+            --     name = 'buffer',
+            --     option = {
+            --         get_bufnrs = function()
+            --             local bufs = {}
+            --             for _, win in ipairs(vim.api.nvim_list_wins()) do
+            --                 bufs[vim.api.nvim_win_get_buf(win)] = true
+            --             end
+            --             return vim.tbl_keys(bufs)
+            --         end
+            --     }
+            -- },
     })
 })
 
