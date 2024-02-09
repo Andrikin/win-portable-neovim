2d1801c3ee107786b24c811478694bd46aa71342
Andrikin <contasecretaalternativa@gmail.com>
2024-02-09 04:03:04 -0300

    refac(utils): remover fonte em flashdrive

--- nvim/config/nvim/lua/andrikin/utils.lua
+++ nvim/config/nvim/lua/andrikin/utils.lua
@@ -7,6 +7,209 @@
 ---@field win7 string | nil
 local Utils = {}
 
+---@class Diretorio
+---@field diretorio string Caminho completo do diretório
+local Diretorio = {}
+
+Diretorio.__index = Diretorio
+
+---@param caminho string | table
+---@return Diretorio
+Diretorio.new = function(caminho)
+	vim.validate({caminho = {caminho, {'table', 'string'}}})
+	if type(caminho) == 'table' then
+		for _, valor in ipairs(caminho) do
+			if type(valor) ~= 'string' then
+				error('Diretorio: new: Elemento de lista diferente de "string"!')
+			end
+		end
+	end
+	local diretorio = setmetatable({
+        diretorio = '',
+    }, Diretorio)
+	if type(caminho) == 'table' then
+		local concatenar = caminho[1]
+		for i=2, #caminho do
+			concatenar = concatenar .. diretorio._suffix(caminho[i])
+		end
+		caminho = concatenar
+	end
+	diretorio.diretorio = diretorio._sanitize(caminho)
+	return diretorio
+end
+
+---@private
+---@param str string
+---@return string
+Diretorio._sanitize = function(str)
+    local sanitarizado = ''
+	vim.validate({ str = {str, 'string'} })
+	sanitarizado = string.gsub(str, '/', '\\')
+    return sanitarizado
+end
+
+---@private
+---@param str string
+---@return string
+Diretorio._suffix = function(str)
+	vim.validate({ str = {str, 'string'} })
+	return (str:match('^[/\\]') or str == '') and str or '\\' .. str
+end
+
+---@param caminho string | table
+Diretorio.add = function(self, caminho)
+	if type(caminho) == 'table' then
+		local concatenar = ''
+		for _, c in ipairs(caminho) do
+			concatenar = concatenar .. Diretorio._suffix(c)
+		end
+		caminho = concatenar
+	end
+	self.diretorio = self.diretorio .. Diretorio._suffix(caminho)
+end
+
+---@param other Diretorio | string
+---@return Diretorio
+Diretorio.__div = function(self, other)
+    local nome = self.diretorio
+	if getmetatable(other) == Diretorio then
+        other = other.diretorio
+    elseif type(other) ~= 'string' then
+		error('Diretorio: __div: Elementos precisam ser do tipo "string".')
+	end
+	return Diretorio.new(Diretorio._sanitize(nome .. Diretorio._suffix(other)))
+end
+
+---@param str string
+---@return string
+Diretorio.__concat = function(self, str)
+	if getmetatable(self) ~= Diretorio then
+		error('Diretorio: __concat: Objeto não é do tipo Diretorio.')
+	end
+	if type(str) ~= 'string' then
+		error('Diretorio: __concat: Argumento precisa ser do tipo "string".')
+	end
+	return Diretorio._sanitize(self.diretorio .. Diretorio._suffix(str))
+end
+
+---@return string
+Diretorio.__tostring = function(self)
+	return self.diretorio
+end
+
+Utils.Diretorio = Diretorio
+
+---@type Diretorio
+Utils.OPT = Utils.Diretorio.new(vim.env.NVIM_OPT)
+
+---@class Curl
+---@field unzip_link string Url para download de unzip.exe
+local Curl = {}
+
+Curl.__index = Curl
+
+Curl.new = function()
+    if vim.fn.executable('curl') == 0 then -- verificar se curl está instalado no sistema
+        error('curl: instalado: Não foi encontrado curl no sistema. Verificar e realizar a instalação do curl neste computador!\nLink para download: https://curl.se/windows/latest.cgi?p=win64-mingw.zip')
+    end
+    local curl = setmetatable({
+        unzip_link = 'http://linorg.usp.br/CTAN/systems/win32/w32tex/unzip.exe'
+    }, Curl)
+    curl:bootstrap()
+    return curl
+end
+
+-- FATO: Windows 10 build 17063 or later is bundled with tar.exe which is capable of working with ZIP files 
+---@private
+Curl.bootstrap = function(self)
+	-- Realizar o download da ferramenta unzip
+	if Utils.win7 and vim.fn.executable('tar') == 0 then
+		Utils.notify('Curl: bootstrap: Sistema não possui tar.exe! Realizar a instalação do programa.')
+		do return end
+	end
+	if vim.fn.executable('unzip') == 1 then
+		Utils.notify('Curl: bootstrap: Sistema já possui Unzip.')
+		do return end
+	end
+	self.download(self.unzip_link, Utils.OPT.diretorio)
+	local unzip = vim.fs.find('unzip.exe', {path = Utils.OPT.diretorio, type = 'file'})[1]
+    if vim.v.shell_error > 0 then
+        error('Curl: bootstrap: Não foi possível realizar o download do unzip.exe')
+    elseif unzip == '' then
+		error('Curl: bootstrap: Não foi possível encontrar o executável unzip.exe.')
+	end
+end
+
+---@param link string
+---@param diretorio string
+Curl.download = function(link, diretorio)
+	vim.validate({
+		link = {link, 'string'},
+		diretorio = {diretorio, 'string'}
+	})
+	if link == '' or diretorio == '' then
+		error('Curl: download: Variável nula')
+	end
+	local arquivo = vim.fn.fnamemodify(link, ':t')
+	diretorio = tostring(Utils.Diretorio.new(diretorio) / arquivo)
+	vim.fn.system({
+		'curl',
+		'--fail',
+		'--location',
+		'--silent',
+		'--output',
+		diretorio,
+		link
+	})
+	if vim.v.shell_error == 0 then
+		Utils.notify(string.format('Curl: download: Arquivo %s baixado!', arquivo))
+	else
+		Utils.notify(string.format('Curl: download: Não foi possível realizar o download do arquivo %s!', arquivo))
+	end
+end
+
+---@param arquivo string
+---@param diretorio string
+Curl.extrair = function(arquivo, diretorio)
+	vim.validate({
+		arquivo = {arquivo, 'string'},
+		diretorio = {diretorio, 'string'}
+	})
+	if arquivo == '' or diretorio == '' then
+		error('Curl: extrair: Variárvel nula.')
+	end
+	local extencao = arquivo:match('%.(tar)%.[a-z.]*$') or arquivo:match('%.([a-z]*)$')
+    local extracao = false
+	if extencao == 'zip' then
+		vim.fn.system({
+			'unzip',
+			arquivo,
+			'-d',
+			diretorio
+		})
+        extracao = true
+	elseif extencao == 'tar' then
+		vim.fn.system({
+			'tar',
+			'-xf',
+			arquivo,
+			'-C',
+			diretorio
+		})
+        extracao = true
+	end
+    if extracao then
+        local nome = arquivo:match('[/\\]([^/\\]+)$') or arquivo
+        if vim.v.shell_error == 0 then
+            Utils.notify(string.format('Curl: extrair: Arquivo %s extraído com sucesso!', nome))
+        else
+            Utils.notify(string.format('Curl: extrair: Erro encontrado! Não foi possível extrair o diretorio_arquivo %s', nome))
+        end
+    end
+end
+
+Utils.Curl = Curl
+
 ---@class Registrador
 ---@field diretorio Diretorio Onde as dependências ficaram instaladas
 ---@field deps table Configurações do Registrador
@@ -117,6 +320,8 @@ Registrador.setup = function(self, cfg)
 	self:init()
 end
 
+Utils.Registrador = Registrador
+
 -- Instalação da fonte SauceCodePro no computador
 ---@class SauceCodePro
 ---@field diretorio Diretorio Onde a fonte será instalada
@@ -128,16 +333,18 @@ local SauceCodePro = {}
 
 SauceCodePro.__index = SauceCodePro
 
+SauceCodePro.registro = Utils.Diretorio.new('HKCU') / 'Software' / 'Microsoft' / 'Windows NT' / 'CurrentVersion' / 'Fonts'
+
+SauceCodePro.diretorio = Utils.OPT / 'fonte'
+
+SauceCodePro.link = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/SourceCodePro.zip'
+
+SauceCodePro.arquivo = SauceCodePro.diretorio .. vim.fn.fnamemodify(SauceCodePro.link, ':t')
+
 ---@return SauceCodePro
 SauceCodePro.new = function()
-    local diretorio = Utils.OPT / 'fonte'
-    local link = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/SourceCodePro.zip'
     local fonte = setmetatable({
-        diretorio = diretorio,
-        link = link,
-        arquivo = diretorio .. vim.fn.fnamemodify(link, ':t'),
-        registro = Utils.Diretorio.new('HKCU') / 'Software' / 'Microsoft' / 'Windows NT' / 'CurrentVersion' / 'Fonts',
-        fontes = vim.fn.glob(tostring(diretorio / 'SauceCodePro*.ttf'), false, true),
+        fontes = vim.fn.glob(tostring(SauceCodePro.diretorio / 'SauceCodePro*.ttf'), false, true),
     }, SauceCodePro)
     fonte:bootstrap()
     return fonte
@@ -202,19 +409,33 @@ SauceCodePro.extrair = function(self)
 end
 
 ---Verificando se a fonte está intalada no computador
+---@private
 ---@return boolean
 SauceCodePro.instalado = function(self)
-	local lista = vim.tbl_filter(
-		function(elemento)
-			return elemento:match('SauceCodePro')
+	return #self:query_regedit() > 0
+end
+
+---@private
+---@return table
+SauceCodePro.query_regedit = function(self)
+    local query = vim.tbl_filter(
+		function(entrada)
+			return entrada:match('SauceCodePro')
 		end,
 		vim.fn.systemlist({
 			'reg',
 			'query',
 			self.registro.diretorio,
 			'/s'
-	}))
-	return #lista > 0
+        })
+    )
+    local fontes = vim.tbl_filter(
+		function(fonte)
+			return fonte:match('(.:.*%.ttf)')
+		end,
+        query
+    )
+    return fontes
 end
 
 ---Registra as fontes no regedit do sistema Windows.
@@ -243,15 +464,15 @@ SauceCodePro.baixada = function(self)
 end
 
 --- Desinstala a fonte no regedit do sistema Windows.
-SauceCodePro.remover_regedit = function(self)
-	for _, fonte in ipairs(self.fontes) do
+SauceCodePro.remover_regedit = function()
+	for _, fonte in ipairs(SauceCodePro:query_regedit()) do
 		local nome = vim.fn.fnamemodify(fonte, ':t')
 		nome = nome:match('(.*)%..*$')
 		if nome then
 			vim.fn.system({
 				'reg',
 				'delete',
-				self.registro.diretorio,
+				SauceCodePro.registro.diretorio,
 				'/v',
 				nome,
 				'/f'
@@ -281,201 +502,7 @@ SauceCodePro.instalar = function(self)
 	end
 end
 
----@class Curl
----@field unzip_link string Url para download de unzip.exe
-local Curl = {}
-
-Curl.__index = Curl
-
-Curl.new = function()
-    if vim.fn.executable('curl') == 0 then -- verificar se curl está instalado no sistema
-        error('curl: instalado: Não foi encontrado curl no sistema. Verificar e realizar a instalação do curl neste computador!\nLink para download: https://curl.se/windows/latest.cgi?p=win64-mingw.zip')
-    end
-    local curl = setmetatable({
-        unzip_link = 'http://linorg.usp.br/CTAN/systems/win32/w32tex/unzip.exe'
-    }, Curl)
-    curl:bootstrap()
-    return curl
-end
-
--- FATO: Windows 10 build 17063 or later is bundled with tar.exe which is capable of working with ZIP files 
----@private
-Curl.bootstrap = function(self)
-	-- Realizar o download da ferramenta unzip
-	if Utils.win7 and vim.fn.executable('tar') == 0 then
-		Utils.notify('Curl: bootstrap: Sistema não possui tar.exe! Realizar a instalação do programa.')
-		do return end
-	end
-	if vim.fn.executable('unzip') == 1 then
-		Utils.notify('Curl: bootstrap: Sistema já possui Unzip.')
-		do return end
-	end
-	self.download(self.unzip_link, Utils.OPT.diretorio)
-	local unzip = vim.fs.find('unzip.exe', {path = Utils.OPT.diretorio, type = 'file'})[1]
-    if vim.v.shell_error > 0 then
-        error('Curl: bootstrap: Não foi possível realizar o download do unzip.exe')
-    elseif unzip == '' then
-		error('Curl: bootstrap: Não foi possível encontrar o executável unzip.exe.')
-	end
-end
-
----@param link string
----@param diretorio string
-Curl.download = function(link, diretorio)
-	vim.validate({
-		link = {link, 'string'},
-		diretorio = {diretorio, 'string'}
-	})
-	if link == '' or diretorio == '' then
-		error('Curl: download: Variável nula')
-	end
-	local arquivo = vim.fn.fnamemodify(link, ':t')
-	diretorio = tostring(Utils.Diretorio.new(diretorio) / arquivo)
-	vim.fn.system({
-		'curl',
-		'--fail',
-		'--location',
-		'--silent',
-		'--output',
-		diretorio,
-		link
-	})
-	if vim.v.shell_error == 0 then
-		Utils.notify(string.format('Curl: download: Arquivo %s baixado!', arquivo))
-	else
-		Utils.notify(string.format('Curl: download: Não foi possível realizar o download do arquivo %s!', arquivo))
-	end
-end
-
----@param arquivo string
----@param diretorio string
-Curl.extrair = function(arquivo, diretorio)
-	vim.validate({
-		arquivo = {arquivo, 'string'},
-		diretorio = {diretorio, 'string'}
-	})
-	if arquivo == '' or diretorio == '' then
-		error('Curl: extrair: Variárvel nula.')
-	end
-	local extencao = arquivo:match('%.(tar)%.[a-z.]*$') or arquivo:match('%.([a-z]*)$')
-    local extracao = false
-	if extencao == 'zip' then
-		vim.fn.system({
-			'unzip',
-			arquivo,
-			'-d',
-			diretorio
-		})
-        extracao = true
-	elseif extencao == 'tar' then
-		vim.fn.system({
-			'tar',
-			'-xf',
-			arquivo,
-			'-C',
-			diretorio
-		})
-        extracao = true
-	end
-    if extracao then
-        local nome = arquivo:match('[/\\]([^/\\]+)$') or arquivo
-        if vim.v.shell_error == 0 then
-            Utils.notify(string.format('Curl: extrair: Arquivo %s extraído com sucesso!', nome))
-        else
-            Utils.notify(string.format('Curl: extrair: Erro encontrado! Não foi possível extrair o diretorio_arquivo %s', nome))
-        end
-    end
-end
-
----@class Diretorio
----@field diretorio string Caminho completo do diretório
-local Diretorio = {}
-
-Diretorio.__index = Diretorio
-
----@param caminho string | table
----@return Diretorio
-Diretorio.new = function(caminho)
-	vim.validate({caminho = {caminho, {'table', 'string'}}})
-	if type(caminho) == 'table' then
-		for _, valor in ipairs(caminho) do
-			if type(valor) ~= 'string' then
-				error('Diretorio: new: Elemento de lista diferente de "string"!')
-			end
-		end
-	end
-	local diretorio = setmetatable({
-        diretorio = '',
-    }, Diretorio)
-	if type(caminho) == 'table' then
-		local concatenar = caminho[1]
-		for i=2, #caminho do
-			concatenar = concatenar .. diretorio._suffix(caminho[i])
-		end
-		caminho = concatenar
-	end
-	diretorio.diretorio = diretorio._sanitize(caminho)
-	return diretorio
-end
-
----@private
----@param str string
----@return string
-Diretorio._sanitize = function(str)
-    local sanitarizado = ''
-	vim.validate({ str = {str, 'string'} })
-	sanitarizado = string.gsub(str, '/', '\\')
-    return sanitarizado
-end
-
----@private
----@param str string
----@return string
-Diretorio._suffix = function(str)
-	vim.validate({ str = {str, 'string'} })
-	return (str:match('^[/\\]') or str == '') and str or '\\' .. str
-end
-
----@param caminho string | table
-Diretorio.add = function(self, caminho)
-	if type(caminho) == 'table' then
-		local concatenar = ''
-		for _, c in ipairs(caminho) do
-			concatenar = concatenar .. Diretorio._suffix(c)
-		end
-		caminho = concatenar
-	end
-	self.diretorio = self.diretorio .. Diretorio._suffix(caminho)
-end
-
----@param other Diretorio | string
----@return Diretorio
-Diretorio.__div = function(self, other)
-    local nome = self.diretorio
-	if getmetatable(other) == Diretorio then
-        other = other.diretorio
-    elseif type(other) ~= 'string' then
-		error('Diretorio: __div: Elementos precisam ser do tipo "string".')
-	end
-	return Diretorio.new(Diretorio._sanitize(nome .. Diretorio._suffix(other)))
-end
-
----@param str string
----@return string
-Diretorio.__concat = function(self, str)
-	if getmetatable(self) ~= Diretorio then
-		error('Diretorio: __concat: Objeto não é do tipo Diretorio.')
-	end
-	if type(str) ~= 'string' then
-		error('Diretorio: __concat: Argumento precisa ser do tipo "string".')
-	end
-	return Diretorio._sanitize(self.diretorio .. Diretorio._suffix(str))
-end
-
----@return string
-Diretorio.__tostring = function(self)
-	return self.diretorio
-end
+Utils.SauceCodePro = SauceCodePro
 
 --- Mostra notificação para usuário, registrando em :messages
 ---@param msg string
@@ -510,16 +537,5 @@ Utils.npcall = vim.F.npcall
 
 ---@type string | nil
 Utils.win7 = string.match(vim.loop.os_uname()['version'], 'Windows 7')
-
-Utils.Diretorio = Diretorio
-
-Utils.Curl = Curl
-
-Utils.SauceCodePro = SauceCodePro
-
-Utils.Registrador = Registrador
-
----@type Diretorio
-Utils.OPT = Utils.Diretorio.new(vim.env.NVIM_OPT)
 
 return Utils
