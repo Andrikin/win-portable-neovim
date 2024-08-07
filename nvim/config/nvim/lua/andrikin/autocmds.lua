4fb7fdc077cc4f82668ac55b93a5998fb4732c3d
Andrikin <contasecretaalternativa@gmail.com>
2024-08-07 18:56:26 -0300

    refac(autocmds): recarregar statusline

--- nvim/config/nvim/lua/andrikin/autocmds.lua
+++ nvim/config/nvim/lua/andrikin/autocmds.lua
@@ -319,7 +319,7 @@ autocmd(
         once = true,
 		callback = function()
             vim.cmd.cd('$HOMEPATH/Desktop')
-            vim.cmd.redraw({bang = true})
+            vim.cmd.redrawstatus({bang = true})
 		end,
 	}
 )
