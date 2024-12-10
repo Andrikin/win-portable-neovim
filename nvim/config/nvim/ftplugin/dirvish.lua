8ecbbf69b50c0cc2f1a1c611184bddd360a84312
Andrikin <contasecretaalternativa@gmail.com>
2024-12-10 13:13:27 -0300

    refac(dirvish): autocmds nao execute arquivos
    Que tenham a exteção declarada em $PATHEXT.

--- nvim/config/nvim/ftplugin/dirvish.lua
+++ nvim/config/nvim/ftplugin/dirvish.lua
@@ -1,6 +1,5 @@
 if not vim.b.did_dirvish then
     local buf = vim.api.nvim_get_current_buf()
-    local opts = { expr = true, buffer = buf }
     vim.keymap.set('n', '.', function()
         local cmd = ':<c-u>! '
         if vim.fn.empty(vim.fn.getline(".")) == 1 then
@@ -11,10 +10,11 @@ if not vim.b.did_dirvish then
         -- finalizando map
         cmd = cmd .. '<home><c-right>'
         return cmd
-    end, opts)
+    end, { expr = true, buffer = buf })
     vim.keymap.set('n', 'go', function()
         local arquivo = vim.fn.getline('.'):gsub('\\', '\\/'):gsub('\\/$', ''):gsub('\\$', '')
-        if vim.fn.fnamemodify(arquivo, ':e') ~= '' and vim.fn.isdirectory(arquivo) == 0 then
+        local extencao = vim.fn.fnamemodify(arquivo, ':e')
+        if (extencao ~= '' or vim.env.PATHEXT:lower():match(extencao)) and vim.fn.isdirectory(arquivo) == 0 then
             vim.fn.jobstart(
                 vim.fn.shellescape(arquivo, 1),
                 {detach = true}
