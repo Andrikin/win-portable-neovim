647b0f523d045741e8c4d7ddd5e3448731a7043e
Andrikin <contasecretaalternativa@gmail.com>
2024-12-16 18:48:55 -0300

    refac(ftplugin): voltar para buffer anterior

--- nvim/config/nvim/after/ftplugin/checkhealth.lua
+++ nvim/config/nvim/after/ftplugin/checkhealth.lua
@@ -2,11 +2,12 @@ if vim.b.did_checkhealth then
     do return end
 end
 vim.keymap.set('n', 'gq', function()
-    local id = vim.fn.gettabinfo(vim.fn.tabpagenr())[1].windows[1]
+    -- local id = vim.fn.gettabinfo(vim.fn.tabpagenr())[1].windows[1]
     vim.cmd.quit()
-    if id then
-        vim.fn.win_gotoid(id) -- ir para a primeira window da tab
-    end
+    vim.fn.feedkeys('\\<c-w>p', 'n')
+    -- if id then
+    --     vim.fn.win_gotoid(id) -- ir para a primeira window da tab
+    -- end
 end,
     { silent = true, buffer = vim.api.nvim_get_current_buf()}
 )
