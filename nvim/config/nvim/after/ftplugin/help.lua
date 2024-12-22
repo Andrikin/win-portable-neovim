9e32a0154754dc8f7e0dbdcd54360db76c493925
Andrikin <contasecretaalternativa@gmail.com>
2024-12-21 23:19:16 -0300

    fix(ftplugin): retornar para janela anterior
    Comentários removidos do arquivo harpoon.

--- nvim/config/nvim/after/ftplugin/help.lua
+++ nvim/config/nvim/after/ftplugin/help.lua
@@ -1,9 +1,6 @@
 vim.keymap.set('n', 'gq', function()
-    local id = vim.fn.gettabinfo(vim.fn.tabpagenr())[1].windows[1]
     vim.cmd.quit()
-    if id then
-        vim.fn.win_gotoid(id) -- ir para a primeira window da tab
-    end
+    vim.fn.feedkeys('\\<c-w>p', 'n')
 end,
     { silent = true, buffer = vim.api.nvim_get_current_buf()}
 )
