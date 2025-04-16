5fe1e6bd3fc38403e4ecc4959ebe825db68a1c38
Andrikin <contasecretaalternativa@gmail.com>
2025-04-16 12:22:05 -0300

    feat(treesitter): desabilitar quando arquivo é grande

--- nvim/config/nvim/lua/andrikin/lsp.lua
+++ nvim/config/nvim/lua/andrikin/lsp.lua
@@ -35,6 +35,15 @@ if vim.fn.executable('x86_64-w64-mingw32-gcc') == 1 then
                 },
                 highlight = {
                     enable = true,
+                    ---@diagnostic disable-next-line: unused-local
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        ---@diagnostic disable-next-line: undefined-field
+                        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
                     additional_vim_regex_highlighting = true,
                 },
                 indent = { enable = true },
