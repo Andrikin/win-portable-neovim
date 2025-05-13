c28afb16795e875d720f453956ef189e9f980fc2
Andrikin <contasecretaalternativa@gmail.com>
2025-05-13 12:35:25 -0300

    refac(undotree): removido diff window

--- nvim/config/nvim/lua/andrikin/options.lua
+++ nvim/config/nvim/lua/andrikin/options.lua
@@ -107,8 +107,8 @@ vim.g.traces_num_range_preview = 0
 vim.g.undotree_WindowLayout = 1
 vim.g.undotree_ShortIndicators = 1
 vim.g.undotree_SetFocusWhenToggle = 1
-vim.g.undotree_DiffpanelHeight = 10
--- vim.g.undotree_DiffAutoOpen = 1
+vim.g.undotree_DiffAutoOpen = 0
+-- vim.g.undotree_DiffpanelHeight = 10
 
 -- --- Netrw ---
 -- Disable Netrw
