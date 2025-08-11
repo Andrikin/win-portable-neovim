49235a86c9c18f284cc93819b545d688685652dd
Andrikin <contasecretaalternativa@gmail.com>
2025-08-11 14:14:23 -0300

    refac(ftplugin): close command

--- nvim/config/nvim/after/ftplugin/checkhealth.lua
+++ nvim/config/nvim/after/ftplugin/checkhealth.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', 'gq', function()
     -- local id = vim.fn.gettabinfo(vim.fn.tabpagenr())[1].windows[1]
-    vim.cmd.quit()
-    vim.fn.feedkeys('\\<c-w>p', 'n')
+    vim.cmd.close()
+    vim.fn.feedkeys('\\<c-w>p', 'n') -- voltar para window anterior
     -- if id then
     --     vim.fn.win_gotoid(id) -- ir para a primeira window da tab
     -- end
--- nvim/config/nvim/after/ftplugin/help.lua
+++ nvim/config/nvim/after/ftplugin/help.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', 'gq', function()
-    vim.cmd.quit()
-    vim.fn.feedkeys('\\<c-w>p', 'n')
+    vim.cmd.close()
+    vim.fn.feedkeys('\\<c-w>p', 'n') -- voltar para window anterior
 end,
     { silent = true, buffer = vim.api.nvim_get_current_buf()}
 )
