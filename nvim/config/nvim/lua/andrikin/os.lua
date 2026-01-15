6a1d3ae3c09dad6bb45ebdf074aefa31590f3656
Andrikin <contasecretaalternativa@gmail.com>
2026-01-15 12:38:54 -0300

    refac(os): copyq name server

--- nvim/config/nvim/lua/andrikin/os.lua
+++ nvim/config/nvim/lua/andrikin/os.lua
@@ -1,5 +1,5 @@
 -- Inicializar variavel de ambiente para remote server (Windows 11)
-local copyq = '\\\\.\\pipe\\nvim-copyq'
+local copyq = '\\\\.\\pipe\\copyq'
 local servers = vim.fn.serverlist()
 for _, server in ipairs(servers) do
     if server == copyq then
