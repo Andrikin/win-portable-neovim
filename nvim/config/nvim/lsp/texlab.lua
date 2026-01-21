7ce624c2d0cd8dd1f44e30130aedb8c0750281c7
Andrikin <contasecretaalternativa@gmail.com>
2026-01-21 15:21:39 -0300

    refac(texlab): remove unwanted configuration

--- nvim/config/nvim/lsp/texlab.lua
+++ nvim/config/nvim/lsp/texlab.lua
@@ -4,15 +4,6 @@ return {
     root_markers = {'.git', '.latexmkrc', '.texlabroot', 'texlabroot', 'Tectonic.toml'},
     single_file_support = true,
     settings = {
-        texlab = {
-            diagnosticsDelay = 300,
-            latexFormatter = 'latexindent',
-            latexindent = {
-                ['local'] = nil, -- local is a reserved keyword
-                modifyLineBreaks = false,
-            },
-            bibtexFormatter = 'texlab',
-            formatterLineLength = 80,
-        },
+        texlab = {},
     },
 }
