c19f12f0f07067e15b94f5481516176692bb140e
Andrikin <contasecretaalternativa@gmail.com>
2026-02-19 18:51:32 -0300

    feat(ui2): cmdline experimental
    Somente com neovim nightly.

--- nvim/config/nvim/lua/andrikin/pack.lua
+++ nvim/config/nvim/lua/andrikin/pack.lua
@@ -172,6 +172,19 @@ vim.cmd.packadd('telescope.nvim')
 vim.cmd.packadd('nvim.difftool')
 vim.cmd.packadd('nvim.undotree')
 
+-- experimental
+if vim.version().minor >= 12 then
+    require('vim._core.ui2').enable({
+     enable = true, -- Whether to enable or disable the UI.
+     msg = { -- Options related to the message module.
+       ---@type 'cmd'|'msg' Where to place regular messages, either in the
+       ---cmdline or in a separate ephemeral message window.
+       target = 'cmd',
+       timeout = 4000, -- Time a message is visible in the message window.
+     },
+    })
+end
+
 -- colorizer.lua
 require('colorizer').setup({'css', 'html', 'javascript'})
 -- Lazydev -- Neovim 0.11
