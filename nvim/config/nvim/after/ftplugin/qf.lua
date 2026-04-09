6665bd4d605a0c211691535cda23d15aea36c880
Andrikin <contasecretaalternativa@gmail.com>
2026-04-09 16:10:29 -0300

    fix(quickfix): dois tipos de nome para 'quickfix'
    PORQUÊ MARIA!?

---
+++ nvim/config/nvim/after/ftplugin/qf.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', 'gq', function()
+    local id = vim.api.nvim_get_current_win()
+    vim.cmd.wincmd('p') -- voltar para window anterior
+    vim.api.nvim_win_close(id, true)
+end,
+    { silent = true, buffer = vim.api.nvim_get_current_buf()}
+)
+vim.treesitter.start()
+
