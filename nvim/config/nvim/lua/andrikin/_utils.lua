4fc8925ca52bb382cc1caafad31380c3e6c24c9c
Andrikin <contasecretaalternativa@gmail.com>
2026-06-11 16:28:54 -0300

    wip: use vim.fs.find function

--- nvim/config/nvim/lua/andrikin/_utils.lua
+++ nvim/config/nvim/lua/andrikin/_utils.lua
@@ -5,9 +5,9 @@ vim.env.MYVIMDIR = vim.fs.joinpath(
 	vim.env.HOME, 'nvim'
 )
 
-M.optsfile = vim.fs.joinpath(
+M.optfile = vim.fs.joinpath(
 	vim.env.MYVIMDIR,
-	'opts', 'optsfile'
+	'opt', 'optfile'
 )
 
 -- append to the last
@@ -15,29 +15,35 @@ local add_path = function(dir)
 	vim.env.PATH = vim.env.PATH .. ';' .. dir
 end
 
-local create_optsfile = function()
-	local opts = vim.fs.dirname(M.optsfile)
-	if not vim.uv.fs_stat(opts) then
-		error('Não foi encontrado diretório "opts" do Neovim')
+local create_optfile = function()
+	local opt = vim.fs.dirname(M.optfile)
+	if not vim.uv.fs_stat(opt) then
+		error('Não foi encontrado diretório "opt" do Neovim')
 	end
-	-- ponto crítico, de mais demora no primeiro 'run'
-	local optslist = vim.fn.glob((opts .. '/*/**/*.exe'),
-		false, true, false
-	)
-	optslist = vim.tbl_map(function(programa)
+	-- ponto crítico, de mais demora na primeira execução
+	-- local optlist = vim.fn.glob((opts .. '/*/**/*.exe'),
+	-- 	false, true, false
+	-- )
+    local optlist = vim.fs.find(
+        function(n, _)
+            return n:match('.*%.exe$')
+        end,
+        {limit = math.huge, type = 'file', path = opt}
+    )
+	optlist = vim.tbl_map(function(programa)
 		return vim.fs.dirname(programa)
-	end, optslist)
-	optslist = vim.list.unique(optslist)
-	vim.fn.writefile(optslist, M.optsfile)
+	end, optlist)
+	optlist = vim.list.unique(optlist)
+	vim.fn.writefile(optlist, M.optfile)
 	vim.notify('Arquivo OPTSFILE criado com sucesso!')
 end
 
 -- inicializar variavéis do ambiente $PATH
 M.init_path = function(force)
-	if not vim.uv.fs_stat(M.optsfile) or force then
-		create_optsfile()
+	if not vim.uv.fs_stat(M.optfile) or force then
+		create_optfile()
 	end
-	local opts = vim.fn.readfile(M.optsfile)
+	local opts = vim.fn.readfile(M.optfile)
 	for _, o in ipairs(opts) do
 		add_path(o)
 	end
