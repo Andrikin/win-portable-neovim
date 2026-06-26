50c8f7eaf32d817e546119c1acd8896507a008a4
Andrikin <contasecretaalternativa@gmail.com>
2026-06-26 18:18:23 -0300

    fix(autocmds): ui2 and setting Andrikin group

--- nvim/config/nvim/lua/andrikin/autocmds.lua
+++ nvim/config/nvim/lua/andrikin/autocmds.lua
@@ -1,38 +1,41 @@
--- Autocmds goosebumps
 local autocmd = vim.api.nvim_create_autocmd
--- local Andrikin = vim.api.nvim_create_augroup('Andrikin', { clear = true })
+local Andrikin = vim.api.nvim_create_augroup('Andrikin', { clear = true })
 
 -- Highlight linha quando entrar em INSERT MODE
 autocmd('InsertEnter', {
+    group = Andrikin,
     pattern = '*',
-    callback = function(w)
-        local dirvish = vim.bo[w.buf].ft == 'dirvish' -- não desativar quando for Dirvish
+    callback = function()
+        local dirvish = vim.bo.ft == 'dirvish' -- não desativar quando for Dirvish
         if dirvish then
             return
         end
-        vim.wo[vim.api.nvim_get_current_win()][0].cursorline = true
-    end,
-})
-autocmd('WinEnter', {
-    pattern = '*',
-    callback = function()
-        vim.wo[vim.api.nvim_get_current_win()][0].cursorline = false
+        vim.wo.cursorline = true
     end,
 })
 autocmd('InsertLeave', {
+    group = Andrikin,
     pattern = '*',
-    callback = function(w)
-        local dirvish = vim.bo[w.buf].ft == 'dirvish' -- não desativar quando for Dirvish
+    callback = function()
+        local dirvish = vim.bo.ft == 'dirvish' -- não desativar quando for Dirvish
         if dirvish then
             return
         end
-        vim.wo[vim.api.nvim_get_current_win()][0].cursorline = false
+        vim.wo.cursorline = false
+    end,
+})
+autocmd('WinEnter', {
+    group = Andrikin,
+    pattern = '*',
+    callback = function()
+        vim.wo.cursorline = false
     end,
 })
 
 -- Resize windows automatically
 -- Tim Pope goodness
 autocmd('VimResized', {
+    group = Andrikin,
     pattern = '*',
     callback = function()
         vim.cmd.wincmd('=')
@@ -41,6 +44,7 @@ autocmd('VimResized', {
 
 -- Highlight configuração
 autocmd('TextYankPost', {
+    group = Andrikin,
     pattern = '*',
     callback = function()
         vim.hl.hl_op({
@@ -52,6 +56,7 @@ autocmd('TextYankPost', {
 
 -- Remover fonte do regedit (Windows)
 autocmd('VimLeave', {
+    group = Andrikin,
     callback = function()
         local flashdrive = vim.env.HOME:sub(1, 1):lower() ~= 'c'
         local remover = false
@@ -73,6 +78,7 @@ autocmd('VimLeave', {
 -- --- Builtin LSP commands ---
 -- Only available in git projects (git init)
 autocmd('LspAttach', {
+    group = Andrikin,
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client and client:supports_method('textDocument/completion') then
@@ -90,21 +96,19 @@ autocmd('LspAttach', {
 -- ideia: copiar todo texto quando sair do buffer, criando nova entrada no
 -- copyq: tab -> clipboard
 autocmd('BufWrite', {
+    group = Andrikin,
     pattern = 'Copyq*.txt',
-    callback = function(args)
-        vim.bo[args.buf].fixendofline = false
-        vim.bo[args.buf].endofline = false
-        -- vim.bo[args.buf].fileformat = 'dos'
+    callback = function()
+        vim.bo.fixendofline = false
+        vim.bo.endofline = false
+        vim.bo.fileformat = 'dos'
         vim.cmd.yank({reg = '+', range = {1, vim.fn.line('$')}})
     end
 })
 
-autocmd('VimEnter',
-	{
-		callback = function()
-			-- experimental: ui2
-			require('vim._core.ui2').enable()
-		end,
-	}
-)
+autocmd('UIEnter', {
+    group = Andrikin,
+    -- experimental: ui2
+    callback = require('vim._core.ui2').enable
+})
 
