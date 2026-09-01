f3dd5bc53e17c7e8924efa9488371218d10ff11a
Andrikin <contasecretaalternativa@gmail.com>
2026-09-01 18:46:22 -0300

    refac(python): set uv command to run python code

--- nvim/config/nvim/after/ftplugin/python.lua
+++ nvim/config/nvim/after/ftplugin/python.lua
@@ -2,5 +2,10 @@ vim.treesitter.start()
 vim.schedule(function ()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 end)
-vim.bo.makeprg = 'python3 %:S'
+if vim.fn.executable('uv') == 1 then
+    vim.bo.makeprg = 'uv run %:S'
+elseif vim.fn.executable('python') == 1 then
+    vim.bo.makeprg = 'python3 %:S'
+end
+vim.cmd.compiler('pyunit')
 
