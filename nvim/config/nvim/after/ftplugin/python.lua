81d14e24f29534f9c1d909b6007de9f5ebaf5136
Andrikin <contasecretaalternativa@gmail.com>
2026-09-01 16:08:47 -0300

    fix(python): this option is local to buffer

--- nvim/config/nvim/after/ftplugin/python.lua
+++ nvim/config/nvim/after/ftplugin/python.lua
@@ -1,7 +1,6 @@
-local buf = vim.api.nvim_get_current_buf()
 vim.treesitter.start()
 vim.schedule(function ()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 end)
-vim.wo.makeprg = 'python3 %:S'
+vim.bo.makeprg = 'python3 %:S'
 
