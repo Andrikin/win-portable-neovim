f4f11841b058e45893303e9150c747c3e3c182ed
Andrikin <contasecretaalternativa@gmail.com>
2026-09-09 15:12:05 -0300

    refac(firenvim): add mappings for movements

--- nvim/config/nvim/lua/andrikin/firenvim.lua
+++ nvim/config/nvim/lua/andrikin/firenvim.lua
@@ -155,14 +155,20 @@ vim.keymap.set('n', 'gP', '"+P')
 vim.keymap.set('n', 'gp', '"+p')
 vim.keymap.set({'n', 'v'}, 'gy', '"+y')
 vim.keymap.set('n', 'gY', '"+Y')
-vim.keymap.set('i', '<up>', "<c-o>:m.-2<cr>", {silent = true})
-vim.keymap.set('i', '<down>', "<c-o>:m.+1<cr>", {silent = true})
-vim.keymap.set('n', '<up>', ":m.-2<cr>", {silent = true})
-vim.keymap.set('n', '<down>', ":m.+1<cr>", {silent = true})
-vim.keymap.set('v', 'K', ":m'<-2<cr>gv", {silent = true})
-vim.keymap.set('v', 'J', ":m'>+1<cr>gv", {silent = true})
--- gJ com o mesmo comportamento de J (juntar linhas removendo espaços)
-vim.keymap.set('v', 'gJ', ":<c-u>'<,'>join<cr>", {silent = true})
+
+-- <up/down>: melhor quando utilizado com mapeamento das teclas <a-j/k> em
+-- nível de sistema
+vim.keymap.set('i', '<up>', "<c-o>:<c-u>m.-2<cr>", {silent = true})
+vim.keymap.set('i', '<down>', "<c-o>:<c-u>m.+1<cr>", {silent = true})
+vim.keymap.set('n', '<up>', ":<c-u>m.-2<cr>", {silent = true})
+vim.keymap.set('n', '<down>', ":<c-u>m.+1<cr>", {silent = true})
+vim.keymap.set('v', '<up>', ":<c-u>'<,'>m'<-2<cr>gv", {silent = true})
+vim.keymap.set('v', '<down>', ":<c-u>'<,'>m'>+1<cr>gv", {silent = true})
+-- CURSOR: move one word to left or right
+vim.keymap.set('c', '<left>', '<s-left>')
+vim.keymap.set('c', '<right>', '<s-right>')
+vim.keymap.set('i', '<left>', '<s-left>')
+vim.keymap.set('i', '<right>', '<s-right>')
 
 -- 'gk' e 'gj' ThePrimeagen way
 local van_halen = function(acao)
