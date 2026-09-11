d07d040ac58713f0851769e3f2ce4acf7490e146
Andrikin <contasecretaalternativa@gmail.com>
2026-09-11 12:30:43 -0300

    refac: remove unused 'buf' variable

--- nvim/config/nvim/after/ftplugin/html.lua
+++ nvim/config/nvim/after/ftplugin/html.lua
@@ -1,4 +1,3 @@
-local buf = vim.api.nvim_get_current_buf()
 vim.treesitter.start()
 if not vim.g.loaded_emmet_vim then
     vim.cmd.packadd('emmet-vim')
--- nvim/config/nvim/after/ftplugin/javascript.lua
+++ nvim/config/nvim/after/ftplugin/javascript.lua
@@ -1,4 +1,3 @@
-local buf = vim.api.nvim_get_current_buf()
 vim.treesitter.start()
 vim.schedule(function ()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
