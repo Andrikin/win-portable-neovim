959e08ed80fa48c3c841312cfafc8de0764c74ff
Andrikin <contasecretaalternativa@gmail.com>
2026-09-11 23:40:57 -0300

    feat(syntax/qf): FINALLY!!!
    Obscurecer grande parte do diretório do arquivo. 'quickfix' será
    'navegado', portanto não há necessidade de ver o caminho inteiro...

--- nvim/config/nvim/after/syntax/qf.lua
+++ nvim/config/nvim/after/syntax/qf.lua
@@ -2,10 +2,7 @@ vim.wo.conceallevel = 2
 vim.wo.concealcursor = 'nvc'
 vim.wo.wrap = false
 vim.schedule(function ()
-    -- vim.cmd.syntax('match qfFileNameConceal =^[^|]*/= contained conceal')
-    -- funciona
-    vim.cmd.syntax('match qfFileNameConceal =^.*/\\ze[^/|]*= conceal')
-    -- vim.cmd.syntax('clear qfFileName')
-    -- vim.cmd.syntax('match qfFileName /^[^|]*/ contains=qfFileNameConceal nextgroup=qfSeparator1 links to Directory')
+    vim.cmd.syntax('match qfFileNameConceal =.*/= contained nextgroup=qfFileNameConcel,qfFileName conceal')
+    vim.cmd.syntax('match qfFileName /^[^|]*/ contains=qfFileNameConceal nextgroup=qfSeparator1')
 end)
 
