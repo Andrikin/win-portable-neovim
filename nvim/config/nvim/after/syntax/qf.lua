96a771ae453d811b782b6c7868ba035f82d2bf8e
Andrikin <contasecretaalternativa@gmail.com>
2026-09-12 01:22:45 -0300

    fix: some lines was getting futher conceal

--- nvim/config/nvim/after/syntax/qf.lua
+++ nvim/config/nvim/after/syntax/qf.lua
@@ -2,7 +2,7 @@ vim.wo.conceallevel = 2
 vim.wo.concealcursor = 'nvc'
 vim.wo.wrap = false
 vim.schedule(function ()
-    vim.cmd.syntax('match qfFileNameConceal =.*/= contained nextgroup=qfFileNameConcel,qfFileName conceal')
+    vim.cmd.syntax('match qfFileNameConceal =[^|]\\{-}/= contained nextgroup=qfFileNameConcel,qfFileName conceal')
     vim.cmd.syntax('match qfFileName /^[^|]*/ contains=qfFileNameConceal nextgroup=qfSeparator1')
 end)
 
